f0d29144ca63e287180652611de5c36696dbc0b8
Md. Shohidujjaman Sajib <sajib1615018@stud.kuet.ac.bd>
2023-01-08 16:00:44 +0600

    another failed attemp for social media sharing

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -2,13 +2,25 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router";
 import Button from "../components/button/Button";
-
+import { Helmet } from "react-helmet";
 const UserDetails = () => {
   const navigate = useNavigate();
   const { user_type, id } = useParams();
   const userData = useSelector((state) => state.userDetails.data);
   return (
     <div>
+      <Helmet>
+        <title>Details of {userData?.first_name}</title>
+        <meta
+          property="og:title"
+          content={`Details of {userData?.first_name}`}
+        />
+        <meta
+          property="og:image"
+          content={"https://api.unsplash.com/search/photos?page=1&query=man"}
+        />
+        <meta property="og:description" content={"Test"} />
+      </Helmet>
       <h5>Details of {user_type}:</h5>
       <table>
         <tr>
@@ -43,9 +55,20 @@ const UserDetails = () => {
       <div>
         <button>
           <a
-            href={`https://www.facebook.com/sharer.php?u=${window.location.href}`}
+            href={`https://www.facebook.com/sharer.php?u=pocoegg.com`}
+            // href={`https://www.facebook.com/sharer.php?u=${window.location.href}`}
           >
             Facebook
+          </a>
+        </button>
+        <button>
+          <a
+            href={`https://twitter.com/share?url=pocoegg.com&text=${userData?.first_name}`}
+            // href={`https://www.facebook.com/sharer.php?u=${window.location.href}`}
+            rel="noreferrer"
+            target="_blank"
+          >
+            Twitter
           </a>
         </button>
       </div>
