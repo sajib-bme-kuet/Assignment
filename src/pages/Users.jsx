0ecd29396d2f8371b3b2aaa8b8ae2a532294b5fa
Md. Shohidujjaman Sajib <sajib1615018@stud.kuet.ac.bd>
2023-01-08 09:12:33 +0600

    redux setup

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,22 +1,27 @@
 import Button from "../components/button/Button";
+import UserDetailsForm from "../components/forms/UserDetailsForm";
 import Modal from "../components/modal/Modal";
 import Tab from "../components/tab/Tab";
+import Table from "../components/table/Table";
 import Tabs from "../components/tabs/Tabs";
 
-import { useState } from "react";
-import UserDetailsForm from "../components/forms/UserDetailsForm";
-import Table from "../components/table/Table";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Users = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [activeTab, setActiveTab] = useState();
 
+  useEffect(() => {
+    axios.get();
+  }, [activeTab]);
   return (
     <>
       <Modal open={isOpen} onClose={() => setIsOpen(false)} title="Add User">
         <UserDetailsForm />
       </Modal>
       <Button onClick={() => setIsOpen(true)}>Add User</Button>
-      <Tabs>
+      <Tabs activeTab={activeTab} setActiveTab={setActiveTab}>
         <Tab label={"Employee"}>
           <Table
             columns={[
@@ -37,8 +42,6 @@ const Users = () => {
               { label: "First Name", fieldName: "first_name" },
               { label: "Last Name", fieldName: "last_name" },
               { label: "User Type", fieldName: "user_type" },
-              // { label: "Division", fieldName: "division" },
-              // { label: "District", fieldName: "istrict" },
             ]}
             rows={[{ id: 1, name: "Admin" }]}
           />
